6ac623d1ed97af26c07b9d8c975b25253adeaa5d
malashkovv <malashkovv@gmail.com>
2022-01-08 12:22:02 +0300

    Separated init and added volumes

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,13 +6,18 @@ export HADOOP_AWS_PATH=$(ls ${HADOOP_HOME}/share/hadoop/tools/lib/ | grep hadoop
 
 export HADOOP_CLASSPATH=${HADOOP_LIB_PATH}/${AWS_SDK_PATH}:${HADOOP_LIB_PATH}/${HADOOP_AWS_PATH}
 
+run_init_schema() {
+  exec /opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType mysql
+}
+
 run_metastore() {
-  /opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType mysql
   exec /opt/apache-hive-metastore-3.0.0-bin/bin/start-metastore
 }
 
 if [ $1 == 'metastore' ]; then
   run_metastore
+elif [  $1 == 'init'  ]; then
+  run_init_schema
 fi
 
 exec "$@"
